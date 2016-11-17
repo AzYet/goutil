4fc0d9cad235f15fe8cbf39531482f04555e9d51
PC_Chen <pengcheng.chen@baishancloud.com>
2016-11-17 16:16:22 +0800

    add new logger and ticker

---
+++ log.go
@@ -0,0 +1,35 @@
+package go_utils
+
+import (
+	"github.com/x-cray/logrus-prefixed-formatter"
+	"github.com/getsentry/raven-go"
+	"github.com/evalphobia/logrus_sentry"
+	"github.com/Sirupsen/logrus"
+)
+
+func NewLogrusWithSentryHook(DSL, release string) *logrus.Logger {
+	l := logrus.New()
+	fmtr := &prefixed.TextFormatter{}
+	fmtr.TimestampFormat = "2006/01/02 15:04:05"
+	l.Formatter = fmtr
+	l.Level = logrus.DebugLevel
+
+	if DSL != "" {
+		client, err := raven.New("abc")
+		client.SetRelease("123")
+		if err != nil {
+			l.Errorf("failed to create raven agent %v.", err)
+		} else if h, err := logrus_sentry.NewWithClientSentryHook(client, []logrus.Level{
+			logrus.PanicLevel,
+			logrus.FatalLevel,
+			logrus.ErrorLevel,
+		}); err != nil {
+			l.Errorf("failed to create hook %v.", err)
+		} else {
+			l.Hooks.Add(h)
+			l.Infoln("sentry hook added.")
+		}
+	}
+	return l
+}
+
---
+++ time.go
@@ -0,0 +1,50 @@
+package go_utils
+
+import (
+	"time"
+	"github.com/Sirupsen/logrus"
+)
+
+type TickMinSec struct {
+	C <-chan time.Time
+	s chan int
+}
+
+func (t TickMinSec)Stop() {
+	t.s <- 1
+}
+
+func TickAtSecondPoint(interval time.Duration, delay time.Duration, Logger *logrus.Logger) TickMinSec {
+	tc := make(chan time.Time, 1)
+	tms := TickMinSec{tc, make(chan int, 1) }
+	ts := time.Now()
+	var tick *time.Ticker
+	go func() {
+		if sub := ts.Sub(ts.Truncate(interval)) - delay; sub >= 0 {
+			//Logger.Printf("exceed %v, start now.", ts.Truncate(interval).Add(delay))
+			tc <- ts
+			time.Sleep(ts.Add(interval).Truncate(interval).Add(delay).Sub(ts))
+			tick = time.NewTicker(interval)
+			tc <- time.Now()
+		} else {
+			Logger.Printf("first task will start at %v.", ts.Truncate(interval).Add(delay).Format("2006-01-02 15:04:05"))
+			time.Sleep(ts.Truncate(interval).Add(delay).Sub(ts))
+			tick = time.NewTicker(interval)
+			tc <- time.Now()
+		}
+		for {
+			select {
+			case <-tms.s:
+				tick.Stop()
+				return
+			case <-tick.C:
+					select {
+					default:
+					case tc <- time.Now():
+					}
+			}
+		}
+	}()
+	return tms
+}
+
