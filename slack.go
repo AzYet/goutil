43bea81f81913b055f140474e02fbd3c3423e615
pengcheng.chen <pengcheng.chen@baishancloud.com>
2016-11-27 13:01:20 +0800

    slack: use struct instead of map in data struct

--- slack.go
+++ slack.go
@@ -8,11 +8,41 @@ import (
 	"github.com/Sirupsen/logrus"
 )
 
+type Field struct {
+	Title string `json:"title"`
+	Value string `json:"value"`
+	Short bool   `json:"short"`
+}
+
+type Attachment struct {
+	Fallback   *string  `json:"fallback"`
+	Color      *string  `json:"color"`
+	PreText    *string  `json:"pretext"`
+	AuthorName *string  `json:"author_name"`
+	AuthorLink *string  `json:"author_link"`
+	AuthorIcon *string  `json:"author_icon"`
+	Title      *string  `json:"title"`
+	TitleLink  *string  `json:"title_link"`
+	Text       *string  `json:"text"`
+	ImageUrl   *string  `json:"image_url"`
+	Fields     []*Field `json:"fields"`
+	Footer     *string  `json:"footer"`
+	FooterIcon *string  `json:"footer_icon"`
+}
+
 type SlackData struct {
-	Text        string `json:"text"`
-	UserName    string `json:"username"`
-	IconUrl     string `json:"icon_url"`
-	Attachments []map[string]string `json:"attachments"`
+	Parse       string       `json:"parse,omitempty"`
+	Username    string       `json:"username,omitempty"`
+	IconUrl     string       `json:"icon_url,omitempty"`
+	IconEmoji   string       `json:"icon_emoji,omitempty"`
+	Channel     string       `json:"channel,omitempty"`
+	Text        string       `json:"text,omitempty"`
+	Attachments []Attachment `json:"attachments,omitempty"`
+}
+
+func (attachment *Attachment) AddField(field Field) *Attachment {
+	attachment.Fields = append(attachment.Fields, &field)
+	return attachment
 }
 
 func FeedSlack(d *SlackData, Logger *logrus.Logger) {
